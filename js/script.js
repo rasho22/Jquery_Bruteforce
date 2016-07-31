3fd47f6cd1bbc02eadb46183fd5e4a84fb2275c5
rasho22 <ab_rasho@yahoo.fr>
2016-07-31 14:44:53 +0200

    mise en page html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	/*var passeword= $("#passeword");
+		$.post(""){
+			passeword:passeword
+		}, function(data)
+	 $.ajax({url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt", success: function(result){
+       $("#resultat").html(result);
+   }});*/
+
+});
+
