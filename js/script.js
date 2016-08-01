b284e365c1b2630fda70c501935440fdd3672f77
rasho22 <ab_rasho@yahoo.fr>
2016-08-01 22:39:51 +0200

    Plusieurs tentatives, mais sans ……. git add *git add *!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,54 @@
 $(function(){
+	var txt="";
+	$.ajax({
+		url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt",
+		type : 'POST',
+		type : 'GET',
+       dataType : 'html',
+        success : function(code_html, statut){
+       },
+       error : function(resultat, statut, erreur){
+
+       },
+       complete : function(resultat, statut){
+       	 $("#resultat").html(txt);
+
+       }
+
+	});
+
+});
+
+	/*
+
+	var txt="";
+	$.ajax({
+		url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt",
+		success: function(){
+			txt +="success \n";
+		},
+		error: function(){
+			txt += "error \n";
+		},
+		beforeSend:function(){
+			txt +="beforeSend \n";
+		},
+		dataFilter : function(){
+			txt +="dataFilter \n";
+		},
+		complete : function(){
+			txt +="complete \n";
+			 $("#resultat").html(txt);
+		},
+	})*/
 	/*var passeword= $("#passeword");
 		$.post(""){
 			passeword:passeword
 		}, function(data)
-	 $.ajax({url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt", success: function(result){
+	 $.ajax({url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt",
+	  success: function(result){
        $("#resultat").html(result);
    }});*/
 
-});
+
 
